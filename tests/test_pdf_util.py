07f6850bb821ee53ebfa1cbfec977fd8369d7eac
Paul Baclace <peb@pebmac4.localdomain>
2020-02-22 18:30:35 -0800

    adjustment for pdf_util.extract_pdf_image() returning an array

--- tests/test_pdf_util.py
+++ tests/test_pdf_util.py
@@ -2,6 +2,7 @@
 import os
 
 from pdf_trio import pdf_util
+import numbers as np
 
 
 def test_extract_pdf_text():
@@ -20,12 +21,15 @@ def test_extract_pdf_image():
     test_pdf_path = 'tests/files/research/submission_363.pdf'
     with open(test_pdf_path, 'rb') as f:
         pdf_content = f.read()
-        img_path = pdf_util.extract_pdf_image(pdf_content, test_pdf_path)
-    assert img_path
-    img_path = pdf_util.extract_pdf_image(pdf_content, test_pdf_path, page=2)
-    assert img_path
-
-    assert test_pdf_path in img_path
-    assert img_path.endswith('.jpg')
-    os.unlink(img_path)
-
+        img_as_array = pdf_util.extract_pdf_image(pdf_content, test_pdf_path)
+    # shape is (299, 299, 3)
+    assert img_as_array
+    assert img_as_array.shape[0] == 299
+    assert img_as_array.shape[1] == 299
+    assert img_as_array.shape[2] == 3
+    img_as_array = pdf_util.extract_pdf_image(pdf_content, test_pdf_path, page=2)
+    # shape is (299, 299, 3)
+    assert img_as_array
+    assert img_as_array.shape[0] == 299
+    assert img_as_array.shape[1] == 299
+    assert img_as_array.shape[2] == 3
